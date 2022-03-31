94babfc45143681d7df03ac9fb80bcc6050f2e4f
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2022-03-31 15:28:15 +0700

    fix: disble custom build

--- Assets/Scripts/Utils/CustomBuildProcessor.cs
+++ Assets/Scripts/Utils/CustomBuildProcessor.cs
@@ -1,13 +1,13 @@
-using UnityEditor.Build;
-using UnityEditor.Build.Reporting;
-using UnityEngine;
+//using UnityEditor.Build;
+//using UnityEditor.Build.Reporting;
+//using UnityEngine;
 
-public class CustomBuildProcessor : IPreprocessBuildWithReport
-{
-    public int callbackOrder { get { return 0; } }
+//public class CustomBuildProcessor : IPreprocessBuildWithReport
+//{
+//    public int callbackOrder { get { return 0; } }
 
-    public void OnPreprocessBuild(BuildReport report)
-    {
-        Debug.Log("MyCustomBuildProcessor.OnPreprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
-    }
-}
+//    public void OnPreprocessBuild(BuildReport report)
+//    {
+//        Debug.Log("CustomBuildProcessor.OnPreprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
+//    }
+//}
