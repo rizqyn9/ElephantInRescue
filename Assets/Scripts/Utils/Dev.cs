e89699980b0658cd2bc130b553dd1979fbd01e81
rizqyn9 <rizqynugroho9@gmail.com>
2022-07-02 11:00:18 +0700

    feat: development environemnt

--- Assets/Scripts/Utils/Dev.cs
+++ Assets/Scripts/Utils/Dev.cs
@@ -1,65 +1,26 @@
-using System.Collections;
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dev : MonoBehaviour
 {
-    #region Singleton
-    private static Dev _instance;
-    public static Dev Instance { get => _instance; }
-    #endregion
-
-    [Header("Properties")]
-    public bool isDevMode;
-    public bool useCustomUserModel = true;
-    public bool useNewDataUser = false;
-    public PlayerDataModel m_customPlayerModel;
-    public bool useLevelTest = true;
-    public GameObject gameManagerPrefab;
+    [SerializeField] GameObject DevUI;
+    [SerializeField] Text levelTarget, stageTarget;
 
     private void OnEnable()
     {
-        isDevMode = false;
-//#if UNITY_EDITOR
-//        isDevMode = true;  
-//#endif
+#if !DEVELOPMENT_BUILD && !UNITY_EDITOR
+        Destroy(gameObject);        
+#endif
     }
 
-    private void Awake()
+    public void BtnDevTogle()
     {
-        if (_instance != null) Destroy(gameObject);
-        else
-        {
-            _instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
+        DevUI.SetActive(!DevUI.activeInHierarchy);
     }
 
-    private void Start()
+    public void Load()
     {
-        StartCoroutine(IDevStart());
-    }
-
-    IEnumerator IDevStart()
-    {
-        if (isDevMode)
-        {
-            if (GameManager.Instance == null) Instantiate(gameManagerPrefab);
-            print("<color=red> Dev Mode</color>");
-        }
-
-        yield return new WaitUntil(() => GameManager.Instance != null);
-
-        if (GameManager.SceneState == SceneState.MAINMENU)
-        {
-            GameManager.PlayerDataModel = m_customPlayerModel;
-            GameManager.Instance.Initialize();
-
-        }
-        else
-        {
-            //LevelManager.Instance.Init();
-        }
+        GameManager.LoadGameLevel(GameManager.GetLevelDataByLevelStage(int.Parse(levelTarget.text), int.Parse(stageTarget.text)));
     }
 }
 
